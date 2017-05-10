5eca6b40a987530621c978852a694aeafe45aaba
Brent Thomas <brentthomas@Brents-MacBook-Air.local>
2017-05-09 23:54:08 -0600

    creating books

--- db/linkQuery.js
+++ db/linkQuery.js
@@ -10,7 +10,41 @@ function authorInfo(author_id) {
     .where('author_id', author_id)
 }
 
+function newBook(body){
+  console.log(body);
+  console.log('bitches');
+  return knex('books').insert({
+    title: body.title,
+    genre: body.genre,
+    description: body.description,
+    coverPic: body.coverPic
+  })
+}
+
 module.exports = {
   bookInfo,
-  authorInfo
+  authorInfo,
+  newBook
 }
+
+
+// <form class="bookForm" action="/galvanize_reads/library/books" method="post">
+//   <label for="title"></label>
+//   <input type="text" class="newBookTitle" name="title" value="">
+//   <label for="genre"></label>
+//   <input type="text" class="newBookGenre" name="genre" value="">
+//   <label for="coverPic">Cover Image URL</label>
+//   <input type="text" class="newBookTitle" name="coverPic" value="">
+//   <label for="description"></label>
+//   <input type="text" class="newBookDescription" name="description" value="">
+//   <form class="" action="index.html" method="post">
+//     <div class="authorBox"></div>
+//     <div class="deleteAuthorBox"></div>
+//     {{#select authorss}}
+//       <option value="{{fName}}">
+//         {{fName}}
+//       </option>
+//     {{/select}}
+//   </form>
+//   <button type="button" name="button">Add Book</button>
+// </form>
