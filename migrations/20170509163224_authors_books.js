99edcc6f3b7104daadfa60641cbf598c65d2cc3f
Brent Thomas <brentthomas@Brents-MacBook-Air.local>
2017-05-16 12:49:38 -0600

    heroku push

--- migrations/20170509163224_authors_books.js
+++ migrations/20170509163224_authors_books.js
@@ -2,8 +2,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('authors_books', function(table){
     table.increments()
-    table.integer('books_id').references('books.book_id');
-    table.integer('authors_id').references('authors.author_id');
+    table.integer('books_id').references('books.book_id').onDelete('CASCADE');
+    table.integer('authors_id').references('authors.author_id').onDelete('CASCADE');
     })
   }
 
